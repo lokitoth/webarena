a765216efd8d5c70423f5ce5a3abf4f4304b4dc3
Jacob Alber <jacob.alber@microsoft.com>
2024-04-03 15:25:23 +0000

    interop: Make WebArena's openai use compatible with AutoGen's

--- run.py
+++ run.py
@@ -347,7 +347,7 @@ def test(
                     Path(args.result_dir) / "traces" / f"{task_id}.zip"
                 )
 
-        except openai.error.OpenAIError as e:
+        except openai.OpenAIError as e:
             logger.info(f"[OpenAI Error] {repr(e)}")
         except Exception as e:
             logger.info(f"[Unhandled Error] {repr(e)}]")
